29a069c3362cbc17b4d1a78a7bf7affc82d96129
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-03-28 22:17:54 -0400

    Added script files

--- mapd-ce-cpu/build.sh
+++ mapd-ce-cpu/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-mkdir -p $PREFIX
-cp -r * $PREFIX
+mkdir -p $PREFIX/opt/mapd
+cp -r * $PREFIX/opt/mapd
+
+sed -i 's/mapd\.conf\.in/\$MAPD_PATH\/systemd\/mapd\.conf\.in/g' \
+	$PREFIX/opt/mapd/systemd/install_mapd_systemd.sh
+
+mkdir -p $PREFIX/etc/conda/activate.d
+mkdir -p $PREFIX/etc/conda/deactivate.d
+cp $RECIPE_DIR/scripts/activate.sh $PREFIX/etc/conda/activate.d/mapd_env.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $PREFIX/etc/conda/deactivate.d/mapd_env.sh
+
