fa54e11843efb49b893fb330e9e02f6060d201a7
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-03-28 23:04:37 -0400

    Use sed to change text on install_mapd_systemd.sh. First test works fine.

--- mapd-ce-cpu/build.sh
+++ mapd-ce-cpu/build.sh
@@ -6,6 +6,9 @@ cp -r * $PREFIX/opt/mapd
 sed -i 's/mapd\.conf\.in/\$MAPD_PATH\/systemd\/mapd\.conf\.in/g' \
 	$PREFIX/opt/mapd/systemd/install_mapd_systemd.sh
 
+sed -i 's/ \$f/ \$MAPD_PATH\/systemd\/\$f/g' \
+	$PREFIX/opt/mapd/systemd/install_mapd_systemd.sh
+
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
 cp $RECIPE_DIR/scripts/activate.sh $PREFIX/etc/conda/activate.d/mapd_env.sh
