da77c8dff8d3478427932bde030fdf2035eb6c01
Pearu Peterson <pearu.peterson@gmail.com>
2018-05-31 16:59:48 +0300

    WIP: conda package for building mapd-core dependecies.

---
+++ mapd-deps/build.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#env
+
+# Useful environment variables
+# PREFIX - installation prefix
+# RECIPE_DIR - path to recipe directory
+
+
+#
+# Install system packages
+#
+
+# Installing system packages requires sudo
+SYSTEM_PREFIX=/usr
+SUDO=
+if [ ! -w $(dirname $SYSTEM_PREFIX) ] ; then
+    SUDO=sudo
+fi
+
+TARGET=`lsb_release -i -s`-`lsb_release -c -s`
+echo "TARGET=$TARGET" # Ubuntu-xenial, CentOS-Core
+
+COMMON_SYSTEM_PACKAGES="wget make git zlib"
+
+case $TARGET in
+CentOS*)
+  SYSTEM_INSTALL_COMMAND="$SUDO yum install --assumeyes"
+  BOOST_PACKAGES="boost-context \
+                  boost-thread \
+                  boost-program-options \
+                  boost-regex \
+                  boost-system \
+                  boost-chrono \
+                  boost-filesystem"
+  SYSTEM_PACKAGES="$COMMON_SYSTEM_PACKAGES $BOOST_PACKAGES \
+                   zlib-devel \
+                   gmp-devel mpfr-devel libmpc-devel \
+                   gcc-c++ gcc \
+                   "
+  ;;
+Ubuntu*)
+  SYSTEM_INSTALL_COMMAND="$SUDO apt install --assume-yes"
+  BOOST_PACKAGES="libboost-context-dev \
+                   libboost-thread-dev \
+                   libboost-program-options-dev \
+                   libboost-regex-dev \
+                   libboost-system-dev \
+                   libboost-chrono-dev \
+                   libboost-filesystem-dev"
+  SYSTEM_PACKAGES="$COMMON_SYSTEM_PACKAGES $BOOST_PACKAGES \
+                   cmake \
+                   gcc-5 g++-5 \
+                   "
+  ;;
+*)
+  echo "NOTIMPL TARGET=$TARGET"
+  exit 1
+
+esac
+
+$SYSTEM_INSTALL_COMMAND $SYSTEM_PACKAGES 
+
+#
+# Install packages from source
+#
+
+LOCAL_PREFIX=$PREFIX
+LOCAL_PREFIX=$RECIPE_DIR/local  # temporary
+mkdir -p $LOCAL_PREFIX
+
+AWSCPP_VERSION=1.3.10
+GCC_VERSION=5.5.0
+GCC_PREFIX=$PREFIX  # to be replaced with LOCAL_PREFIX
+
+GCC_PREFIX=$RECIP_DIR/installs  # temporary
+mkdir -p $GCC_PREFIX            # temporary
+
+source $RECIPE_DIR/packages.sh
+
+case $TARGET in
+CentOS*)
+  # CentOS gcc-4.8 is too old:
+  install_gcc 
+  export CC=$GCC_PREFIX/bin/gcc
+  export CXX=$GCC_PREFIX/bin/g++
+  $SUDO yum remove -y gcc-c++ gcc # to make sure that gcc-5 is used
+
+  # CentOS cmake-2.8 is too old:
+  #download_make_install https://internal-dependencies.mapd.com/thirdparty/cmake-3.7.2.tar.gz
+  ;;
+Ubuntu*)
+  export CC=gcc-5
+  export CXX=g++-5
+  ;;
+*)
+esac
+
+#install_awscpp
---
+++ mapd-deps/packages.sh
@@ -0,0 +1,120 @@
+function download() {
+    wget --continue "$1"
+}
+
+function extract() {
+    tar xvf "$1"
+}
+
+function makej() {
+    make -j $(nproc)
+}
+
+function download_make_install() {
+    name="$(basename $1)"
+    download "$1"
+    extract $name
+    if [ -z "$2" ]; then
+        pushd ${name%%.tar*}
+    else
+        pushd $2
+    fi
+
+    if [ -x ./Configure ]; then
+        ./Configure --prefix=$PREFIX $3
+    else
+        ./configure --prefix=$PREFIX $3
+    fi
+    makej
+    make install
+    popd
+}
+
+function download_make_install_local() {
+    name="$(basename $1)"
+    download "$1"
+    extract $name
+    if [ -z "$2" ]; then
+        pushd ${name%%.tar*}
+    else
+        pushd $2
+    fi
+
+    if [ -x ./Configure ]; then
+        ./Configure --prefix=$LOCAL_PREFIX $3
+    else
+        ./configure --prefix=$LOCAL_PREFIX $3
+    fi
+    makej
+    make install
+    popd
+}
+
+
+function install_gcc_deps() {
+  download_make_install https://internal-dependencies.mapd.com/thirdparty/gmp-6.1.2.tar.xz "" "--enable-fat"
+  download_make_install https://internal-dependencies.mapd.com/thirdparty/mpfr-3.1.5.tar.xz "" "--with-gmp=$PREFIX"
+  download_make_install ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz "" "--with-gmp=$PREFIX"
+}
+
+function install_gcc() {
+  #install_gcc_deps
+  download ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
+  extract gcc-$GCC_VERSION.tar.xz
+  pushd gcc-$GCC_VERSION
+  export CPPFLAGS="-I$PREFIX/include"
+  ./configure \
+    --prefix=$GCC_PREFIX \
+    --disable-multilib \
+    --enable-bootstrap \
+    --enable-shared \
+    --enable-threads=posix \
+    --enable-checking=release \
+    --with-system-zlib \
+    --enable-__cxa_atexit \
+    --disable-libunwind-exceptions \
+    --enable-gnu-unique-object \
+    --enable-languages=c,c++ \
+    --with-tune=generic \
+    #--with-gmp=$PREFIX \
+    #--with-mpc=$PREFIX \
+    #--with-mpfr=$PREFIX #replace '--with-tune=generic' with '--with-tune=power8' for POWER8
+  makej
+  make install
+  popd
+}
+
+function install_awscpp() {
+    # default c++ standard support
+    CPP_STANDARD=14
+    # check c++17 support
+    GNU_VERSION1=`g++ --version|head -n1|awk '{print $4}'|cut -d'.' -f1`
+    if [ "$GNU_VERSION1" = "7" ]; then
+        CPP_STANDARD=17
+    fi
+    rm -rf aws-sdk-cpp-${AWSCPP_VERSION}
+    download https://github.com/aws/aws-sdk-cpp/archive/${AWSCPP_VERSION}.tar.gz
+    tar xvfz ${AWSCPP_VERSION}.tar.gz
+    pushd aws-sdk-cpp-${AWSCPP_VERSION}
+    mkdir build
+    cd build
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DBUILD_ONLY="s3" \
+        -DBUILD_SHARED_LIBS=0 \
+        -DCUSTOM_MEMORY_MANAGEMENT=0 \
+        -DCPP_STANDARD=$CPP_STANDARD \
+        ..
+    make $*
+
+    # sudo is needed on osx
+    os=`uname`
+    if [ "$os" = "Darwin" ]; then
+        sudo make install
+    else 
+        make install
+    fi
+
+    popd
+}
