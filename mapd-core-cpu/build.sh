4a553abf688fc1fd47b98f301747f189b1969b2a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-09-04 19:13:08 -0700

    Updated recipe

--- mapd-core-cpu/build.sh
+++ mapd-core-cpu/build.sh
@@ -11,21 +11,13 @@ if [ $(uname) == Darwin ]; then
   export MACOSX_DEPLOYMENT_TARGET="10.9"
 fi
 
-# export LIBGFLAGS_INCLUDE_DIR=$PREFIX/lib
-# export LIBGLOG_INCLUDE_DIR=$PREFIX/lib
-export LD_LIBRARY_PATH=$PREFIX/lib
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-export GDAL_LIBRARY=$PREFIX
-export GDAL_INCLUDE_DIR=$PREFIX/include
+# export CPPFLAGS="-std=c++14 -I$PREFIX/include $CPPFLAGS"
+# export CXXFLAGS="-std=c++14 -I$PREFIX/include $CXXFLAGS"
 
-# export CPPFLAGS="-std=c++14 -I$PREFIX/include"
-# export CXXFLAGS="-std=c++14"
-# cmake -E env LDFLAGS="-rpath=-L$LD_LIBRARY_PATH" cmake ..
-
-CUDA_ROOT=$(ls -d /Developer/NVIDIA/CUDA-* | tail -n 1)
-DYLD_LIBRARY_PATH=$CUDA_ROOT/lib:$DYLD_LIBRARY_PATH
-PATH=$CUDA_ROOT/bin:$PATH
-PATH=$HOME/bin:$PATH
+export CONDA_LIB=$PREFIX/lib
+export LD_LIBRARY_PATH=$CONDA_LIB:$LD_LIBRARY_PATH
+export DYLD_LIBARY_PATH=$CONDA_LIB:$DYLD_LIBRARY_PATH
+export BOOST_INCLUDEDIR=$PREFIX/include
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -38,5 +30,5 @@ cmake \
     -DENABLE_TESTS=on  \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX ..
-make -j 4
 
+make
