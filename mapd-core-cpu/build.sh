8076935f59c3fa270fa998606f16e2ecead1fb4d
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-09-11 09:10:26 -0700

    New updates on recipe

--- mapd-core-cpu/build.sh
+++ mapd-core-cpu/build.sh
@@ -11,13 +11,23 @@ if [ $(uname) == Darwin ]; then
   export MACOSX_DEPLOYMENT_TARGET="10.9"
 fi
 
-# export CPPFLAGS="-std=c++14 -I$PREFIX/include $CPPFLAGS"
-# export CXXFLAGS="-std=c++14 -I$PREFIX/include $CXXFLAGS"
+export CPPFLAGS="-std=c++14 -I$PREFIX/include $CPPFLAGS"
+export CXXFLAGS="-std=c++14 -I$PREFIX/include $CXXFLAGS"
+
+# export CPPFLAGS=""
+# export CXXFLAGS=""
 
 export CONDA_LIB=$PREFIX/lib
 export LD_LIBRARY_PATH=$CONDA_LIB:$LD_LIBRARY_PATH
 export DYLD_LIBARY_PATH=$CONDA_LIB:$DYLD_LIBRARY_PATH
 export BOOST_INCLUDEDIR=$PREFIX/include
+
+# cudda
+# CUDA_ROOT=$(ls -d /Developer/NVIDIA/CUDA-* | tail -n 1)
+# DYLD_LIBRARY_PATH=$CUDA_ROOT/lib:$DYLD_LIBRARY_PATH
+# PATH=$CUDA_ROOT/bin:$PATH
+# PATH=$HOME/bin:$PATH
+# export DYLD_LIBRARY_PATH PATH
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
