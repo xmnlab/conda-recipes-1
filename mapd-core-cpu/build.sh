b1b9388c8dc5ae9128b519d2bbc27bcd02d3646c
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-09-30 16:20:22 -0700

    Fixing test issue

--- mapd-core-cpu/build.sh
+++ mapd-core-cpu/build.sh
@@ -11,23 +11,9 @@ if [ $(uname) == Darwin ]; then
   export MACOSX_DEPLOYMENT_TARGET="10.9"
 fi
 
-export CPPFLAGS="-std=c++14 -I$PREFIX/include $CPPFLAGS"
-export CXXFLAGS="-std=c++14 -I$PREFIX/include $CXXFLAGS"
-
-# export CPPFLAGS=""
-# export CXXFLAGS=""
-
-export CONDA_LIB=$PREFIX/lib
-export LD_LIBRARY_PATH=$CONDA_LIB:$LD_LIBRARY_PATH
-export DYLD_LIBARY_PATH=$CONDA_LIB:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export DYLD_LIBARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
 export BOOST_INCLUDEDIR=$PREFIX/include
-
-# cudda
-# CUDA_ROOT=$(ls -d /Developer/NVIDIA/CUDA-* | tail -n 1)
-# DYLD_LIBRARY_PATH=$CUDA_ROOT/lib:$DYLD_LIBRARY_PATH
-# PATH=$CUDA_ROOT/bin:$PATH
-# PATH=$HOME/bin:$PATH
-# export DYLD_LIBRARY_PATH PATH
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -35,10 +21,12 @@ cmake \
     -DENABLE_CUDA=off \
     -DMAPD_IMMERSE_DOWNLOAD=off \
     -DMAPD_DOCS_DOWNLOAD=off \
-    -DPREFER_STATIC_LIBS=on \
+    -DPREFER_STATIC_LIBS=off \
     -DENABLE_AWS_S3=off \
     -DENABLE_TESTS=on  \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX ..
 
+make ParserFiles
 make
+make install
--- mapd-core-cpu/run_test.sh
+++ mapd-core-cpu/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mapd_server -v
+mapdql -v
